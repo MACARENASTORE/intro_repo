8f89ad5590a3a84401f67370cdfd1c20c6d5a4f4
Diego Barreto <oclusiva9204@gmail.com>
2023-03-03 06:47:58 -0500

    version3

--- repo.js
+++ repo.js
@@ -4,5 +4,6 @@
     u: no se tiene seguimiento (untuacked)
     a: agregardo
     m : modificado
+    checkout volver a una version anterior
     gitlens vsc : ver graficamente caracteristicas del repositorio
 */
