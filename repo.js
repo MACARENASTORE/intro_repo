0a026190568e1d11a4c9b9bad0e97e9606cd8b2e
Diego Barreto <oclusiva9204@gmail.com>
2023-03-03 07:11:25 -0500

    commit5

--- repo.js
+++ repo.js
@@ -4,6 +4,11 @@
     u: no se tiene seguimiento (untuacked)
     a: agregardo
     m : modificado
+    pull : actualizar un repositorio 
     checkout volver a una version anterior
     gitlens vsc : ver graficamente caracteristicas del repositorio
+    git init : iniciar un repositorio
+    git status : estado del repositorio
+    git log : 
+    git remote : estado del repositorio remoto
 */
